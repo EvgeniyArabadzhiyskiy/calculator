0475155a54620efb586031d95cacc51cc5bd618c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-10-31 22:06:26 +0300

    Create colors.js

---
+++ src/theme/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  DARK_GRAY: '#333333',
+  MEDIUM_GRAY: '#bdbdbd',
+  LIGHT_GRAY: '#f5f5f5',
+  WHITE: '#ffffff',
+  BLACK: '#000000',
+  CORAL_LIGHT: '#ff7961',
+  AQUAMARINE: '#00897b',
+  RED: 'red',
+  ERROR: '#ff0000',
+};
+
+export default colors;
