556c015bd4fbfa1e6d89893956e546754f30f0d3
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-01 21:48:59 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,6 @@
-import { useSelector } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
-
-import { GlobalStyles } from 'components/GlobalStyles';
-import { lightTheme, darkTheme, coloredTheme } from '../../theme/theme';
-
-import CalculatorFC from 'components/Calculator';
-import Settings from 'components/Settings';
+import Router from 'components/Router/Router';
 
 const App = () => {
-  const currentTheme = useSelector(state => state.settings.theme);
-
-  const theme = {
-    light: lightTheme,
-    dark: darkTheme,
-    colored: coloredTheme,
-  };
-
-  return (
-    <div>
-      <ThemeProvider theme={theme[currentTheme]}>
-        <Settings />
-        <CalculatorFC />
-        <GlobalStyles />
-      </ThemeProvider>
-    </div>
-  );
+  return <Router />;
 };
 export default App;
