84f02723fc37d7564c5a52973f88872856641855
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-05 22:01:48 +0200

    Update calculatorSlice.js

--- src/store/slice/calculatorSlice.js
+++ src/store/slice/calculatorSlice.js
@@ -3,8 +3,6 @@ import { validateInput } from 'utils/calculateExpression';
 import { calculateExpression } from '../../utils/calculateExpression/calculateExpression';
 
 const initialState = {
-  // expression: [],
-  // data: '',
   display: '',
   history: [],
   isShouldClear: false,
@@ -18,58 +16,40 @@ export const calculatorSlice = createSlice({
     setDisplay: (state, action) => {
       const payload = String(action.payload);
 
-      if (action.payload[0] === '.') {
-        state.display += '0' 
+      const lastValue = state.display.length - 1;
+
+      if (action.payload === '.' && isNaN(state.display[lastValue])) {
+        console.log(isNaN(state.display[lastValue]));
+        if (state.display[lastValue] === '.') {
+          return
+        }
+        state.display += '0';
       }
 
       if (state.display.length < 40 && validateInput(state.display, payload)) {
         if (state.isShouldClear) {
-          // state.display = "";
           state.isShouldClear = false;
         }
 
         state.display += payload;
-        // state.data += payload;
       }
     },
 
-    // setExpression: (state, { payload }) => {
-    //   state.expression.push(state.data);
-    //   state.data = '';
-
-    //   state.expression.push(payload);
-    //   state.display += payload;
-    // },
-
-    // setExpressionByEquals: state => {
-    //   state.expression.push(state.data);
-    //   state.data = '';
-    // },
-
     clearLastNumber: state => {
       state.display = state.display.slice(0, -1);
-      // state.data = state.data.slice(0, -1);
     },
 
     clearDisplay: state => {
       state.display = '';
-
-      // state.data = '';
-      // state.expression = [];
     },
     calculate: state => {
       const [result, history] = calculateExpression(state.display);
-      // console.log("history", history);
-      // console.log("result", result);
 
       if (result === 'Infinity') {
         state.isError = true;
         state.display = 'ERROR';
         return;
       }
-
-      // state.data = result;
-      // state.expression = [];
 
       state.display = result;
       state.history.push(history);
@@ -79,8 +59,6 @@ export const calculatorSlice = createSlice({
     clearError: state => {
       if (state.isError) {
         state.display = '';
-        // state.data = '';
-        // state.expression = [];
         state.isError = false;
       }
     },
