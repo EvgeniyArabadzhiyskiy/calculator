2c819244c84fde596fb7eac78ede2361fa32b8ba
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-08 21:35:54 +0200

    Create index.js

---
+++ src/utils/calculateExpression/index.js
@@ -0,0 +1,13 @@
+import getCalculate from './getCalculate';
+import makeArrayData from './makeArrayData';
+import validateInput from './validateInput';
+import mathСalculations from './mathСalculations';
+import makeMathOperations from './makeMathOperations';
+
+export {
+  getCalculate,
+  makeArrayData,
+  mathСalculations,
+  makeMathOperations,
+  validateInput,
+};
