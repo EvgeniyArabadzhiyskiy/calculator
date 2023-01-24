c9795c9bd08c92fb296ceae05b48b71684984e79
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-24 10:03:28 +0200

    Create About.jsx

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <>
+      <h1>ABOUT MY APP</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, sit
+        beatae a necessitatibus libero maxime sunt, sapiente nostrum ipsa
+        dolorum deleniti optio sed eum tempore. Odio nobis omnis possimus
+        architecto, dolor at aliquid iure quasi ad voluptatibus iusto sit
+        deserunt nesciunt unde, asperiores, magni repellat totam! Voluptates
+        dolores blanditiis itaque consectetur dicta nihil beatae veniam ipsa
+        earum consequatur? Asperiores, eligendi.
+      </p>
+    </>
+  );
+};
+
+export default About;
