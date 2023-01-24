ac0d85663c479d5d44ce885d2600a937108f7df7
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-24 10:04:09 +0200

    Create index.jsx

---
+++ src/components/Calculator/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import * as Styled from './styled';
+
+import DisplayFC from './Display';
+import HistoryFC from './History';
+import KeypadFC from './Keypad';
+
+
+
+
+const CalculatorFC = () => {
+
+  return (
+    <Styled.FlexContainer>
+      <Styled.CalculatorWrapper>
+        <DisplayFC />
+        <KeypadFC />
+      </Styled.CalculatorWrapper>
+      <HistoryFC />
+    </Styled.FlexContainer>
+  );
+};
+
+export default CalculatorFC;
