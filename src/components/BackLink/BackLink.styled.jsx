fe45119b6db19fb5a71f01b7057122673219e1c9
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-24 10:04:23 +0200

    Create BackLink.styled.jsx

---
+++ src/components/BackLink/BackLink.styled.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const BackLink = styled(Link)`
+  display: inline-block;
+  font-size: 25px;
+  color: orangered;
+  background-color: #17b0e8;
+  font-weight: 700;
+  margin: 20px;
+  padding: 8px;
+  border-radius: 5px;
+`;
